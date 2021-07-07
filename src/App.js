b2e7c29a30e82e35d3a587a90c49a45906fc67b9
Dung Trieu <trieudung199@gmail.com>
2021-07-07 11:10:40 +0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import TableComponent from "./table.component"//
+
+
+function App() {
+    return (
+        <div> 
+        <TableComponent />   
+        {/* <TextF /> */}
+        </div>
+    )
+}
+
+export default App
---
+++ src/table.component.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import { Checkbox } from '@material-ui/core';
+import { useState } from 'react';
+import { CheckBox } from '@material-ui/icons';
+
+const TableComponent = () => {
+  const useStyles = makeStyles({
+    table: {
+      minWidth: 650,
+    },
+  });
+
+  const [checked,setChecked] = useState(true)
+  const [checked1,setChecked1] = useState(true)
+
+  const handleCheck1 = (e) => {
+    setChecked1(e.target.checked1)
+  }
+
+  const handleCheck = (e) => {
+    setChecked(e.target.checked)
+  }
+
+  const createData = (id, name , email, group, customer) => {
+    return { id, name , email, group, customer };
+  }
+
+  const rows = [
+    createData('1', 'A', 'a@gmail.com', 'Ax', '4.0'),
+    createData('2', 'B', 'a@gmail.com', 'Bx', '4.3'),
+    createData('3', 'C', 'a@gmail.com', 'Cx', '6.0'),
+    createData('4', 'D', 'a@gmail.com', 'Dx', '4.3'),
+    createData('5', 'E', 'a@gmail.com', 'Ex', '3.9'),
+  ];
+
+    const classes = useStyles();
+
+    return (
+      <TableContainer component={Paper}>
+        <Table className={classes.table} size="small" aria-label="a dense table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">
+                <Checkbox
+                  checked={checked}
+                  onChange={handleCheck}
+                />
+              </TableCell>
+              <TableCell align="center">ID </TableCell>
+              <TableCell align="center">Name</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">Group</TableCell>
+              <TableCell align="center">Customer</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell align="center">
+                  <CheckBox
+                    checked1={checked1}
+                    onChange={handleCheck1}
+                  />
+                </TableCell>
+                <TableCell align="center" component="th" scope="row">
+                  {row.id}
+                </TableCell>
+                <TableCell align="center">{row.name}</TableCell>
+                <TableCell align="center">{row.email}</TableCell>
+                <TableCell align="center">{row.group}</TableCell>
+                <TableCell align="center">{row.customer}</TableCell>
+              </TableRow>
+            )
+            )}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+}
+
+export default TableComponent
